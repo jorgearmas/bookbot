78a05c9b448a064244f952588c709f75e1102b2c
jarmas <jorgearmasc0496@gmail.com>
2025-01-29 13:53:00 -0600

    feaures added

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import re
+
 def main():
     with open('books/frankenstein.txt') as f:
         file_contents = f.read()
@@ -12,6 +14,31 @@ def count_words(text):
     
     print(counter)
 
+def count_char(text):
+    words = re.split(r"(\s+)", text)
+    data_dict = {}
+    
+    for word in words:
+        word = word.lower()
+        counter = 0
+        for character in word:
+            if character in data_dict:
+                counter = data_dict[character]
+                counter += 1
+                data_dict[character] = counter
+            else:
+                counter = 1
+                data_dict[character] = counter
+    
+    return data_dict
+
+def report(dictionary):        
+    for k, v in dictionary.items():
+        if k != ' ' and k != '.' and k != '#':
+            print(f"The '{k}' character was found {v} times")
+
 if __name__ == "__main__":
     text = main()
     count_words(text)
+    dictionary = count_char(text)
+    report(dictionary)
