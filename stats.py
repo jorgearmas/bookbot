bccdc679f29d9984b887b38067e147e95d6a3a8f
JorgeArmas <jorgearmasc96@gmail.com>
2025-08-07 13:04:39 -0600

    charReport function implemented, func generates a report of chars in the book

--- stats.py
+++ stats.py
@@ -3,15 +3,33 @@ def wordCounter(book_contents):
     return len(single_words)
 
 def charCounter(book_contents):
-    single_chars = {}
+    single_chars_dict = {}
     book_contents_lower = book_contents.lower()
     counter = 0
     for character in book_contents_lower:
         
-        if character not in single_chars:
+        if character not in single_chars_dict:
             counter = 1
-            single_chars[character] = counter
+            single_chars_dict[character] = counter
         else:
-            single_chars[character] = single_chars[character] + 1
+            single_chars_dict[character] = single_chars_dict[character] + 1
     
-    return single_chars
+    return single_chars_dict
+
+def charReport(single_chars_dict):
+    report_list = []
+    
+    for item in single_chars_dict:
+        #print(item," -> ",single_chars_dict[item])
+        contents_of_list_dict = {}
+        contents_of_list_dict["char"] = item
+        contents_of_list_dict["num"] = single_chars_dict[item]
+        report_list.append(contents_of_list_dict)
+    
+    report_list.sort(reverse=True, key=sort_on)
+
+    return report_list
+
+def sort_on(items):
+    return items["num"]
+
