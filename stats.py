2796f7059391d756720256393235845a9c0c8635
JorgeArmas <jorgearmasc96@gmail.com>
2025-08-07 08:49:53 -0600

    function charCounter implemented

--- stats.py
+++ stats.py
@@ -1,3 +1,17 @@
 def wordCounter(book_contents):
     single_words = book_contents.split()
     return len(single_words)
+
+def charCounter(book_contents):
+    single_chars = {}
+    book_contents_lower = book_contents.lower()
+    counter = 0
+    for character in book_contents_lower:
+        
+        if character not in single_chars:
+            counter = 1
+            single_chars[character] = counter
+        else:
+            single_chars[character] = single_chars[character] + 1
+    
+    return single_chars
